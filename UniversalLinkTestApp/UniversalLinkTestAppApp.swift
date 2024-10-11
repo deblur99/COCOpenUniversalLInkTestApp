416bc5d04c3b8a3ac042fc575eaaa61f3fc24b5d
deblur99 <deblur99@kakao.com>
2024-10-11 14:57:47 +0900

    Initial Commit

---
+++ UniversalLinkTestApp/UniversalLinkTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversalLinkTestAppApp.swift
+//  UniversalLinkTestApp
+//
+//  Created by 한현민 on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct UniversalLinkTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
